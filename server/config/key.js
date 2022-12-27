aff4783001993687d6b336f657e9223c6a179ea0
ashrth <aashrrita@gmail.com>
2022-12-27 16:23:17 +0530

    init

--- server/config/key.js
+++ server/config/key.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    // "mongodb+srv://robert:gu6faWD!JU2LNwD@mortgagesaas.1yjjgif.mongodb.net/Mortgage?retryWrites=true&w=majority",
+    
     "mongodb+srv://robert:gu6faWD!JU2LNwD@cluster0.2yne8aj.mongodb.net/?retryWrites=true&w=majority"
 };
